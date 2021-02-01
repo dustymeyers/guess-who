4285794926975a1bf9f9c445fb875666261c7de7
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-02-01 16:36:02 -0600

    assigned data attributes of name to each div

--- client.js
+++ client.js
@@ -7,7 +7,7 @@ function readyOn() {
   //append DOM to include items in people array
   for (let i = 0; i < people.length; i++) {
     $('body').append(`
-      <div class="person">
+      <div class="person" data-name="${people[i].name}">
         <img src="https://github.com/${people[i].githubUsername}.png?size=250" alt="Profile image of ${people[i].name}">
       </div>
     `);
@@ -17,8 +17,14 @@ function readyOn() {
   console.log(people[randomPersonIndex].name);
   // append that person's index name to the dom
   $('#name').append(people[randomPersonIndex].name);
+  // when you click on this element check if the right name
+  $(document).on('click', '.person', guessWho);
 }
 
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+function guessWho() {
+  console.log($(this).data());
 }

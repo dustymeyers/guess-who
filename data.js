297689671afc4ef566055c8731c6e7e78af85bc6
dustymeyers <72757972+dustymeyers@users.noreply.github.com>
2021-02-01 15:59:55 -0600

    Initial commit

---
+++ data.js
@@ -0,0 +1,30 @@
+var people = [
+  {
+    name: 'Dev',
+    githubUsername: 'devjanaprime'
+  },
+  {
+    name: 'Dane',
+    githubUsername: 'DoctorHowser'
+  },
+  {
+    name: 'Mary',
+    githubUsername: 'mbmosman'
+  },
+  {
+    name: 'Kris',
+    githubUsername: 'kdszafranski'
+  },
+  {
+    name: 'Edan',
+    githubUsername: 'eschwartz'
+  },
+  {
+    name: 'Casie',
+    githubUsername: 'cassandradanger'
+  },
+  {
+    name: 'Chris',
+    githubUsername: 'christopher-black'
+  },
+];

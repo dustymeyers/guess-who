9eeaca979a894756901bb1fbcbc611eca5f7db2f
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-02-01 17:13:01 -0600

    base function working

--- client.js
+++ client.js
@@ -1,5 +1,7 @@
 $(document).ready(readyOn);
 console.log('Here are all the available people:', people);
+// create a random number to for chosen index
+let randomPersonIndex = randomNumber(0, people.length);
 
 function readyOn() {
   // on load
@@ -7,16 +9,18 @@ function readyOn() {
   //append DOM to include items in people array
   for (let i = 0; i < people.length; i++) {
     $('body').append(`
-      <div class="person" data-name="${people[i].name}">
+      <div class="person" data-username="${people[i].githubUsername}">
         <img src="https://github.com/${people[i].githubUsername}.png?size=250" alt="Profile image of ${people[i].name}">
       </div>
     `);
   }
-  // create a random number to for chosen index
-  let randomPersonIndex = randomNumber(0, people.length);
+
   console.log(people[randomPersonIndex].name);
   // append that person's index name to the dom
   $('#name').append(people[randomPersonIndex].name);
+  // give them the data value for their index
+  $('#name').data('index', randomPersonIndex);
+  console.log($('#name').data('index'));
   // when you click on this element check if the right name
   $(document).on('click', '.person', guessWho);
 }
@@ -26,5 +30,20 @@ function randomNumber(min, max) {
 }
 
 function guessWho() {
-  console.log($(this).data());
+  console.log($(this).data('username'));
+  let chosenPerson = $(this).data('username');
+  let correctPerson = $('#name').data('index');
+  console.log(correctPerson);
+  if (chosenPerson === people[correctPerson].githubUsername) {
+    alert("Good job! You're correct!");
+    let newPersonIndex = randomNumber(0, people.length);
+    // empty name value and change it to a new one
+    $('#name').empty();
+    // append that person's index name to the dom
+    $('#name').append(people[newPersonIndex].name);
+    // give them the data value for their index
+    $('#name').data('index', newPersonIndex);
+  } else {
+    alert("Sorry, that's incorrect.");
+  }
 }
